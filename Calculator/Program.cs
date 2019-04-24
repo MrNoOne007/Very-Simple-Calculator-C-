d0ceb2a86cc8541d4a38f6e8f98dde5b681d57a5
MrNoOne007 <ramicmujo@hotmail.com>
2019-04-24 17:35:19 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double num1;
+            double num2;
+            double result;
+            string operations;
+
+
+
+            Console.WriteLine("Wellcome to simple Calculator\n");
+            Console.WriteLine("Please enter first number\n");
+            num1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number\n");
+            num2 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please choose operation\n (+,-,*,%) ");
+            operations = Console.ReadLine();
+
+
+            if (operations == "+")
+            {
+                result = num1 + num2;
+                Console.Write("Result is:" + result);
+            }
+            if (operations == "-")
+            {
+                result = num1 - num2;
+                Console.Write("Result is:" + result);
+            }
+
+            if (operations == "*")
+            {
+                result = num1 * num2;
+                Console.Write("Result is:" + result);
+            }
+            if (operations == "%")
+            {
+                result = num1 % num2;
+                Console.Write("Result is:" + result);
+            }
+
+
+            Console.Write("\n" + "Press any key to exit");
+            Console.ReadKey();
+
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
